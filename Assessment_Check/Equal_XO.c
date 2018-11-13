d662dbbda604c332797a24ebb94dd9a803891322
Bartosz Wdowiak <wdowibar@localhost.localdomain>
2018-11-13 15:20:27 +0100

    Equal_XO v1.0

--- Assessment_Check/Equal_XO.c
+++ Assessment_Check/Equal_XO.c
@@ -1,12 +1,18 @@
 #include <string.h>
 #include <assert.h>
+#include <stdbool.h>
 
 /**
 * Function checks if a string has the same amount of 'x' and 'o's. Function must be case insensitive.
 */
 bool XO(const char *word, const int length)
 {
-
+	int countX = 0, countO = 0;
+	for(int i=0; i<length; i++){
+		if(word[i]==88 || word[i]==120) countX++;			//jezeli x lub X inkrementuj licznik X'ow
+		else if(word[i]==79 || word[i]==111) countO++;		//jak wyzej z "O"
+	}
+	return countX==countO ? true : false;
 }
 
 void test_cases()
