f13d8bc7fdb1aa3bbd6c217c49855c84715284ab
Bartosz Wdowiak <wdowibar@localhost.localdomain>
2018-11-16 08:56:39 +0100

    Highest_scoring_word v1.0

--- Part_1/Highest_scoring_word.c
+++ Part_1/Highest_scoring_word.c
@@ -1,28 +1,65 @@
 #include <string.h>
 #include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 
-char* highestScoringWord(const char *sentence){
+char* highestScoringWord(char sentence[])
+{
 
-    int lenght = strlen(sentence);
-    int highestScore = -1;
+    int highestScore = -1, currentScore = 0;
+	char *result, *current;
+
+	current = strtok(sentence, " ,.-");
+
+	while (current != NULL)
+	{
+		for	(int i = 0; i < strlen(current); i++)
+		{
+			currentScore+=current[i]-96;
+		}
+
+		if (currentScore>highestScore)
+		{
+			highestScore=currentScore;
+			result=current;
+		}
+
+		current=strtok(NULL, " ,.-");
+		currentScore=0;
+
+	}
+
+	return result;
 
 }
 
 
 void test_cases()
 {
-	char* answer = highestScoringWord("Ala ma kota");
-	assert(answer == true);
+	char input1[] = "aaaaa d";
+	char* answer = highestScoringWord(input1);
+	assert(!strcmp(answer,"aaaaa"));
 
-	answer = highestScoringWord("Dupa zupa kupa");
-	assert(answer == false);
+	char input2[] = "aaaaa e";
+	answer = highestScoringWord(input2);
+	assert(!strcmp(answer,"aaaaa"));
 
-	answer = highestScoringWord("Tieto tieto TiEtO");
-	assert(answer == true);
+	char input3[] = "aaaaa f";
+	answer = highestScoringWord(input3);
+	assert(!strcmp(answer,"f"));
 
-	answer = highestScoringWord("asd asdafdfdsfgad gdfa fafas AAAAAAAAAAAAA");
-	assert(answer == false);
+	char input4[] = "dupa zupa kupa";
+	answer = highestScoringWord(input4);
+	assert(!strcmp(answer,"zupa"));
+
+	char input5[] = "tieto teito itteo";
+	answer = highestScoringWord(input5);
+	assert(!strcmp(answer,"tieto"));
+
+	char input6[] = "zozole ida droga";
+	answer = highestScoringWord(input6);
+	assert(!strcmp(answer,"zozole"));
 }
 
 int main(int argc, char *argv[])
