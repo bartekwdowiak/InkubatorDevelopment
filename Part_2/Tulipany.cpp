1f2f0c0e64c7019946c330e0e4188555f17c1503
Bartosz Wdowiak <wdowibar@localhost.localdomain>
2018-11-19 09:55:02 +0100

    Fixed wrong equation

--- Part_2/Tulipany.cpp
+++ Part_2/Tulipany.cpp
@@ -9,7 +9,7 @@ int remainingSpecies(int patchCount, std::string givenSpecies)
 {
     std::istringstream speciesStream(givenSpecies);
     std::vector<int> speciesVec;
-    int singleSpecies, result;
+    int singleSpecies, result, allSpecies=15000;
 
     while(speciesStream >> singleSpecies)
     {
@@ -17,10 +17,10 @@ int remainingSpecies(int patchCount, std::string givenSpecies)
     }
 
     std::sort(speciesVec.begin(), speciesVec.end());
-    auto last = std::unique(speciesVec.begin(),speciesVec.end())
+    auto last = std::unique(speciesVec.begin(),speciesVec.end());
 
-    result = speciesVec.begin() - last;
-    std::cout<<result<<std::endl;
+    result = allSpecies - std::distance(speciesVec.begin(), last);
+
     return result;
 }
 
@@ -29,17 +29,17 @@ void test_cases()
 	int answer = remainingSpecies(8,"3 6 2 2 4 6 3 7");
 	assert(answer == 14995);
 
-	/*answer = remainingSpecies("");
-	assert(answer == true);
+	answer = remainingSpecies(12, "1 2 3 4 5 6 7 8 9 10 11 12");
+	assert(answer == 14988);
 
-	answer = remainingSpecies("");
-	assert(answer == false);
+	answer = remainingSpecies(10, "1 1 1 1 1 1 6 9 6 9");
+	assert(answer == 14997);
 
-	answer = remainingSpecies("");
-	assert(answer == false);
+	answer = remainingSpecies(2, "1 2");
+	assert(answer == 14998);
 
-	answer = remainingSpecies("");
-	assert(answer == false);*/
+	answer = remainingSpecies(7,"1 2 4 5 5 5 4");
+	assert(answer == 14996);
 }
 
 int main(int argc, char *argv[])
