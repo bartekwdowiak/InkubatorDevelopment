3a1ac0d2692bb3985162a9fea941f80461597cfc
Bartosz Wdowiak <wdowibar@localhost.localdomain>
2018-11-19 13:56:40 +0100

    [Code Review] Changed use of ascii code value into a variable to improve readability

--- Part_1/Highest_scoring_word.c
+++ Part_1/Highest_scoring_word.c
@@ -6,7 +6,7 @@
 
 char* highestScoringWord(char *sentence)
 {
-    int highestScore = -1, currentScore = 0;
+    int highestScore = -1, currentScore = 0, asciiValue=96;
 	char *result = NULL, *current = NULL;
 
 	current = strtok(sentence, " ,.-");
@@ -15,7 +15,7 @@ char* highestScoringWord(char *sentence)
 	{
 		for	(int i = 0; i < strlen(current); i++)
 		{
-			currentScore+=current[i]-96;
+			currentScore+=current[i]-asciiValue;		//equation which allows to calculate the scoring of a letter using ascii code
 		}
 
 		if (currentScore>highestScore)
